57446cbec442a94530bccd050607cb189925daf1
贺博 <hebo@invault.io>
2019-12-09 17:55:00 +0800

    新增samples

---
+++ rlock-spring-boot-samples/src/main/java/dawn/dlock/rlock/RlockSpringBootSamplesApplication.java
@@ -0,0 +1,13 @@
+package dawn.dlock.rlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlockSpringBootSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlockSpringBootSamplesApplication.class, args);
+	}
+
+}
---
+++ rlock-spring-boot-samples/src/test/java/dawn/dlock/rlock/RlockSpringBootSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package dawn.dlock.rlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlockSpringBootSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
